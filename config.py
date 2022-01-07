d7a37861ecd0c6c7324d28ed5c27723f91fad96e
miksuk28 <74315076+miksuk28@users.noreply.github.com>
2022-01-07 07:59:41 +0100

    changed formatting

--- config.py
+++ config.py
@@ -7,25 +7,47 @@ _server_status_codes = {
     2: {"code": 2, "http_code": 503, "message": "Authserver is currently disabled. Please try again later or contact the admin"}
 }
 
-
 config = {
     # Enable or disable Flask server debug mode and output debug info
     # SET TO FALSE IN PRODUCTION ENV.
     "debug": True,
+    
     # Address to host server on
     "address": "127.0.0.1",
+    
     # Port to host server on
     "port": 5000,
+    
     # How many seconds tokens will be valid for
     "token_valid_time": 120,
+    
     # Allow anyone to register without admin permission
     "allow_register": False,
+    
     # Returned by /authstatus endpoint
     "auth_status_message": _server_status_codes,
+    
     # secret_config.py location, currently does nothing
     "secret_config_location": "",
+    
     # Block date which overrides all user blocks
     "global_dissalow_tokens_before": 0,
+    
+    # Username rules
+    "username_rules": {
+        "min_length": 4,
+        "max_length": 30,
+        # Charachters not allowd in username
+        "banned_symbols": ""
+        # Settings this to a string will only allow new username with these charachters, rest will be blocked
+        "whitelist_symbols": "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghjiklmnopqrstuvwxyz1234567890_!#¤%&£$€"
+    }
+    
     # Password rules
-    "password_rules: {"min_length": 8, "require_uppercase": False, "require_special_char": False, "require_num": False}
+    "password_rules: {
+        "min_length": 8,
+        "require_uppercase": False,
+        "require_special_char": False,
+        "require_num": False
+    }
 }
