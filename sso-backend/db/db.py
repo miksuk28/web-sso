2486e1fa3cb2aa4bbac0f539c486dbefff4b7b89
miksuk28 <miksuk28@gmail.com>
2022-01-11 21:14:13 +0100

    started MongoDB rewrite

--- sso-backend/db/db.py
+++ sso-backend/db/db.py
@@ -1,19 +1,22 @@
 from collections import defaultdict
+from logging import debug
 from jwt.exceptions import DecodeError, ExpiredSignatureError, InvalidKeyError, InvalidSignatureError
-from sqlitedict import SqliteDict
 from bcrypt import gensalt
 from time import time
 from random import choice
 from .db_exceptions import *
+import pymongo
 import jwt
 import hashlib
 
 
 class UsersDatabaseWrapper:
     def __init__(self, config, secret_config):
-        self._users_file = config["db_file"]
+        connection_str = f"mongodb://{config['database']['db_username']}:{secret_config['DB_PASSWORD']}@{config['database']['host']}"
         self._debug = config["debug"]
-        self._users = SqliteDict(self._users_file, autocommit=True, tablename="authserver")
+        self._client = pymongo.MongoClient(connection_str)
+        self._db = self._client[config["db_name"]]
+        self._user_col = self._db[config["db_col_name"]]
         self._token_validity = config["token_valid_time"]
         self._SECRET_KEY = secret_config["SECRET_KEY"]
         self._CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
@@ -57,7 +60,7 @@ class UsersDatabaseWrapper:
 
     
     def add_user(self, username, password=None, access_level="user", disallow_tokens_before=0, restrict_access_to=None, block_login=False):
-        if username in self._users.keys():
+        if username in self._user_col:
             raise UserAlreadyExists(username)
 
         salt = gensalt()
@@ -80,7 +83,10 @@ class UsersDatabaseWrapper:
             "registered": self._unixtime()
         }
         
-        self._users[username] = user
+        operation = self._user_col.insert_one(user)
+
+        if self._debug:
+            print(operation)
 
         if reset_password:
             return {"username": username, "access_level": access_level, "password": password}
@@ -90,39 +96,40 @@ class UsersDatabaseWrapper:
 
     def auth(self, token):
         try:
-            try:
-                payload = jwt.decode(
-                    token,
-                    self._SECRET_KEY,
-                    "HS256"
-                )
+            payload = jwt.decode(
+                token,
+                self._SECRET_KEY,
+                "HS256"
+            )
 
-            except ExpiredSignatureError:
-                raise TokenExpired
-            
-            except InvalidSignatureError:
-                raise TokenInvalid
+        except ExpiredSignatureError:
+            raise TokenExpired
+        
+        except InvalidSignatureError:
+            raise TokenInvalid
 
-            except (InvalidKeyError, DecodeError):
-                raise TokenInvalid
+        except (InvalidKeyError, DecodeError):
+            raise TokenInvalid
 
+        user_query = self._user_col.find_one({"username": payload["username"]}, {"block_login": 1})
 
-            if self._users[payload["user"]]["block_login"]:
-                raise UserBlocked(payload['username'])
-
-        except KeyError:
-            raise UserNotFound(payload['username'])
-
-        return payload
+        if user_query is None:
+            # Raise exception if user not in database
+            raise UserNotFound(payload["username"])
+        elif user_query["block_login"]:
+            # Raise exception if login is blocked for that user
+            raise UserBlocked(payload['username'])
+        else:
+            return payload
 
 
     def _user_exists(self, user):
-        try:
-            _ = self._users[user]
-        except KeyError:
-            raise UserNotFound(user)
+        user_query = self._user_col.find_one({"username": user})
 
-        return True
+        if user_query is None:
+            return False
+        else:
+            return True
 
 
     def _check_password(self, username, password):
