7030733d9073f2da3f5361b4c4e15fe203daae60
miksuk28 <74315076+miksuk28@users.noreply.github.com>
2022-05-17 09:29:54 +0200

    Delete verification.py

--- verification.py
+++
@@ -1,8 +0,0 @@
-import db_exceptions as exc
-
-def validate(keys, dict):
-    for key in keys:
-        value = dict.get(key, None)
-
-        if value is None or value == "":
-            raise exc.MissingValue(key)
